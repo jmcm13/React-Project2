b5ca521881c3690d0f1b63aca3522b7cda4f14c5
jmcm13 <joshuamichalmundo@gmail.com>
2023-02-08 21:49:40 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import MenuBar from "./components/MenuBar";
+import Form from "./components/Form";
+import Display from "./components/Display";
 
 class App extends Component {
+  state = {
+    formData: [],
+  };
+
+  getFormData = (props) => {
+    console.log(props);
+    let formData = [...this.state.formData, props];
+    this.setState({
+      formData,
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
+      <>
+        <MenuBar />
+        <Form getFormData={this.getFormData} />
+        <Display formData={this.state.formData} />
+      </>
     );
   }
 }
